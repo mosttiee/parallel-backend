2a9838a0ad17a0dfb1f77b797ce88eb65a392961
Thanapat Juthavantana <thanapat.jutha@gmail.com>
2019-03-18 17:22:33 +0700

    createv server and socket.io

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const socketIO = require('socket.io')
+http = require('http')
+app = express()
+server = http.createServer(app)
+io = socketIO.listen(server)
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello')
+});
+
+app.post('/login', (req, res) => {
+  res.send('login Api')
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('chat', function (msg) {
+        socket.broadcast.emit('chat', msg);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
